c9951f980803643430f02061e9d665a745eaf5b4
Matthew Smith <matthewsmith@Matthews-MacBook-Pro.local>
2023-12-08 23:04:23 -0600

    security concerns

--- api/emailHandler.jsx
+++ api/emailHandler.jsx
@@ -1,7 +1,7 @@
 import sgMail from '@sendgrid/mail';
 
 export default async function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', 'https://ms-react-portfolio-dyql4fgp6-matthewmsmith.vercel.app');
+  res.setHeader('Access-Control-Allow-Origin', '*');
 
   res.setHeader('Access-Control-Allow-Methods', 'POST', 'OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
