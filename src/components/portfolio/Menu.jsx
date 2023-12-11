4e538e226bd8ec6d2120a4d123cfc37c40dce452
Matthew Smith <matthewsmith@Matthews-MacBook-Pro.local>
2023-12-10 22:28:01 -0600

    feature/add portfolio thumbnail

--- src/components/portfolio/Menu.jsx
+++ src/components/portfolio/Menu.jsx
@@ -1,5 +1,5 @@
-import Work1 from "../../assets/work-6.svg";
-import Work2 from "../../assets/work-3.svg";
+import Work1 from "../../assets/work-1.png";
+import Work2 from "../../assets/work-1.png";
 
 const Menu = [
   {
@@ -9,13 +9,7 @@ const Menu = [
     desc: "Easily manage your stock investments with this user-friendly web app. Add, track, and analyze stocks in your portfolio. Stay on top of your financial game!",
     gitHubLink: "https://github.com/mattmarcussmith/ms-react-stockshark",
   },
-  {
-    id: 2,
-    image: Work2,
-    title: "Word Guessing Game",
-    desc: "Challenge your word skills in this word guessing game. Can you guess the word before your chances run out? Try it now!",
-    gitHubLink: "https://github.com/mattmarcussmith/ms-javascript-word-game",
-  },
+
 ];
 
 export default Menu;
